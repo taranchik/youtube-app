9e03d0c0888e31ff22c5f7c1771310942549b3c3
Viacheslav Taranushenko <viataranushenko@gmail.com>
2022-11-12 22:42:15 +0100

    Added tests

--- src/App.js
+++ src/App.js
@@ -9,26 +9,25 @@ const App = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [videos, setVideos] = useState([]);
   const [selectedVideo, setSelectedVideo] = useState(null);
+  const [searchInput, setSearchInput] = useState("");
 
   const handleSearch = async (e) => {
     e.preventDefault();
 
-    setIsLoading(true);
-
-    // fetching videos data from the youtube API
-    await axios
-      .get(
-        `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${e.target.search.value}&key=${process.env.REACT_APP_YOUTUBE_API_KEY}`
-      )
-      .then(({ data }) => {
-        setVideos(data.items);
-        console.log(data.items);
-
-        if (data.items.length) {
-          setSelectedVideo(data.items[0]);
-        }
-      })
-      .catch(({ response }) => console.log(response.data));
+    if (searchInput) {
+      // fetching videos data from the youtube API
+      await axios
+        .get(
+          `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${searchInput}&key=${process.env.REACT_APP_YOUTUBE_API_KEY}`
+        )
+        .then(({ data }) => {
+          setVideos(data.items);
+          if (data.items.length) {
+            setSelectedVideo(data.items[0]);
+          }
+        })
+        .catch(({ response }) => console.log(response.data));
+    }
 
     setIsLoading(false);
   };
@@ -37,57 +36,68 @@ const App = () => {
     setSelectedVideo(video);
   };
 
+  const handleSearchInputValueChange = (e) => {
+    setSearchInput(e.target.value);
+  };
+
   return (
     <div className="App">
-      <form className="App-Topbar flex-centered" onSubmit={handleSearch}>
+      <div className="App-Topbar flex-centered">
         <input
           name="search"
           placeholder="Search"
           type="text"
-          defaultValue="Starboy"
+          value={searchInput}
+          onChange={handleSearchInputValueChange}
         />
-        <button>{SearchIcon}</button>
-      </form>
-      <div className="App-Main">
-        <div className="App-Main-content">
-          <figure className="video-container">
-            {isLoading ? (
-              Spinner
-            ) : selectedVideo ? (
-              <iframe
-                src={`https://www.youtube.com/embed/${selectedVideo.id.videoId}`}
-                title="YouTube video player"
-                frameBorder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
-              ></iframe>
-            ) : (
-              <div></div>
-            )}
-            <div className="title">
-              {selectedVideo && selectedVideo.snippet.title}
-            </div>
-          </figure>
-          <div className="App-Main-sidebar">
-            {videos.map((video, index) => (
-              <div key={index} className="sidebar-video-container">
-                <div
-                  onClick={handleChangeSelectedVideo.bind(this, video)}
-                  className="thumbnail-wrapper"
-                >
-                  <img
-                    object-position="100% 0"
-                    className="thumbnail"
-                    src={video.snippet.thumbnails.high.url}
-                    alt={`${video.titile}_img`}
-                  />
-                </div>
-                <div>{video.snippet.title}</div>
+        <button onClick={handleSearch}>{SearchIcon}</button>
+      </div>
+      {isLoading ? (
+        Spinner
+      ) : (
+        <div data-testid="main-content-area" className="App-Main">
+          <div className="App-Main-content">
+            <figure className="video-container">
+              {selectedVideo ? (
+                <iframe
+                  src={`https://www.youtube.com/embed/${selectedVideo.id.videoId}`}
+                  title="YouTube video player"
+                  frameBorder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowFullScreen
+                ></iframe>
+              ) : (
+                <div></div>
+              )}
+              <div className="title">
+                {selectedVideo && selectedVideo.snippet.title}
               </div>
-            ))}
+            </figure>
+            <div className="App-Main-sidebar">
+              {videos.map((video, index) => (
+                <div
+                  key={index}
+                  data-testid="sidebar-content-area"
+                  className="sidebar-video-container"
+                >
+                  <div
+                    onClick={handleChangeSelectedVideo.bind(this, video)}
+                    className="thumbnail-wrapper"
+                  >
+                    <img
+                      object-position="100% 0"
+                      className="thumbnail"
+                      src={video.snippet.thumbnails.high.url}
+                      alt={`${video.snippet.title}_img`}
+                    />
+                  </div>
+                  <div>{video.snippet.title}</div>
+                </div>
+              ))}
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,77 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { act, render, screen } from "@testing-library/react";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import App from "./App";
+import axios from "axios";
+import userEvent from "@testing-library/user-event";
+
+const fetchData = async () =>
+  await axios
+    .get(
+      `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=Starboy&key=${process.env.REACT_APP_YOUTUBE_API_KEY}`
+    )
+    .then(({ data }) => data)
+    .catch(({ response }) => console.log(response.data));
+
+describe("<App />", () => {
+  beforeEach(() => {
+    // eslint-disable-next-line testing-library/no-render-in-setup
+    render(<App />);
+
+    const SearchInput = screen.getByPlaceholderText("Search");
+
+    // eslint-disable-next-line testing-library/no-unnecessary-act
+    act(() => {
+      userEvent.type(SearchInput, "Starboy");
+      userEvent.click(screen.getByRole("button"));
+    });
+  });
+
+  it("Should render loading spinner on searching videos", () => {
+    expect(screen.getByTitle("spinner")).toBeInTheDocument();
+  });
+
+  it("Should render main content area after search", async () => {
+    expect(await screen.findByTestId("main-content-area")).toBeInTheDocument();
+  });
+
+  it("Should render list of videos at the side bar", async () => {
+    const data = await fetchData();
+
+    const elementsLength = await screen
+      .findAllByTestId("sidebar-content-area")
+      .then((data) => data.length);
+
+    console.log(elementsLength, data.length);
+
+    expect(elementsLength).toEqual(data.items.length);
+  });
+
+  it("First video from the search list should be selected", async () => {
+    const data = await fetchData();
+    const videoData = data.items[0];
+
+    expect(await screen.findByTitle("YouTube video player")).toHaveAttribute(
+      "src",
+      `https://www.youtube.com/embed/${videoData.id.videoId}`
+    );
+    expect(
+      (await screen.findAllByText(videoData.snippet.title)).length
+    ).toEqual(2);
+  });
+
+  it("Should select second video from the side bar", async () => {
+    const data = await fetchData();
+    const videoData = data.items[1];
+
+    const imgElement = screen.getByAltText(`${videoData.snippet.title}_img`);
+    userEvent.click(imgElement);
+
+    expect(await screen.findByTitle("YouTube video player")).toHaveAttribute(
+      "src",
+      `https://www.youtube.com/embed/${videoData.id.videoId}`
+    );
+    expect(
+      (await screen.findAllByText(videoData.snippet.title)).length
+    ).toEqual(2);
+  });
 });
